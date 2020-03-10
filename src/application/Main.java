f2a6d37fe82a949dfc23450f2d0a1a42d02b578e
Lucasrb18 <lucasrb18@gmail.com>
2020-03-10 12:40:09 -0300

    Starting implement Board and Piece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardGame.Board;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Beggining the chess");
+		Board board = new Board(8, 8);
 		
 	}
 
